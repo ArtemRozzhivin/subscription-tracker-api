01f7db2dd308d9ba320f18d163384b194846965c
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2025-05-25 11:22:25 +0300

    Init project

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { PORT, NODE_ENV } from './config/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
+});
+
+export default app;
